06a071216ad25231d3e2d7c0efdbd8313394d9fa
andaemin <eoals0216@gmail.com>
2025-03-24 20:17:33 +0900

    firstCommit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+        <v-col>
+            <v-text-field v-model="name" label="name"></v-text-field>
+            <v-text-field v-model="pwd" label="Password"></v-text-field>
+            <v-row class="">
+                <v-btn @Click=confirm color="#42A5F5" variant="Tonal" test="test">button</v-btn>
+                <h1>{{ member[0].name }}</h1>
+            </v-row>
+            <div v-for="member in cutList(member)">
+                <div v-if="member.age >= 23">{{ member.name }}의 나이는 {{ member.age }}</div>
+            </div>
+        </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
+export default {
+    name: 'DaeminMain',
+    
+    data(){
+        return{
+        member: [
+                {
+                    name : "nayeon",
+                    age : "25"
+                },
+                {
+                    name : "daHyeon",
+                    age : "24",
+                },
+                {
+                    name : "jeongHyeong",
+                    age : "23"
+                }
+            ]
+        }
+    },
+    methods: {
+        confirm(){
+            alert("onClick");
+            this.test = 'on'
+        },
+        cutList(){
+            const setName = [];
+            this.member.map((list) => {
+                if(list.name == this.name){
+                    setName.push(list);
+                }
+            });
+    }
+    },
+    
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
+</script>
